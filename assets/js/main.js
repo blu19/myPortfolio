f9e6dc18d052456864a2ab5e47920c6ec2fdc01b
BrianU <brian.ulatowski@gmail.com>
2020-04-23 00:25:03 -0700

    update portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function () {
+        $('.menu-toggler').toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $(".top-nav .nav-link").on("click", function () {
+      $('.menu-toggler').removeClass("open");
+      $(".top-nav").removeClass("open");
+    });
+
+    $('nav a[href*="#"]').not("#blog").on('click', function (){
+        $("#nav-link").toggleClass('active');
+        $('html, body').animate({
+           scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+    });
+    $('#up').on('click', function (){
+       $('html, body').animate({
+           scrollTop: 0
+       }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true    
+    });
+});
+
+// $('.top-nav .nav-link').on('click', function() {
+//     $('.menu-toggler').removeClass('open');
+//     $('.top-nav').removeClass('open');
+// })
+
+
+
+
+// phpmailer
